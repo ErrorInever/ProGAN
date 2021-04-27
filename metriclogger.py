0ca72350bb74a2ff79f14f80949ee6c6c1d33e4d
ErrorInever <errorinever@gmail.com>
2021-04-27 23:48:46 +0000

    remove display

--- metriclogger.py
+++ metriclogger.py
@@ -33,33 +33,6 @@ class MetricLogger:
                 'prog_epochs': cfg.PROGRESSIVE_EPOCHS,
             })
 
-    def display_status(self, epoch, num_epochs, batch_idx, num_batches, crt_loss,
-                       gen_loss, acc_real=None, acc_fake=None):
-        """
-        Display training progress
-        :param epoch: ``int``, current epoch
-        :param num_epochs: ``int``, numbers epoch
-        :param batch_idx: ``int``, current batch
-        :param num_batches: ``int``, numbers bathes
-        :param crt_loss: ``torch.autograd.Variable``, discriminator loss
-        :param gen_loss: ``torch.autograd.Variable``, generator loss
-        :param acc_real: ``torch.autograd.Variable``, discriminator predicted on real data
-        :param acc_fake: ``torch.autograd.Variable``, discriminator predicted on fake data
-        """
-        if isinstance(crt_loss, torch.autograd.Variable):
-            crt_loss = crt_loss.item()
-        if isinstance(gen_loss, torch.autograd.Variable):
-            gen_loss = gen_loss.item()
-        if self.show_acc and isinstance(acc_real, torch.autograd.Variable):
-            acc_real = acc_real.float().mean().item()
-        if self.show_acc and isinstance(acc_fake, torch.autograd.Variable):
-            acc_fake = acc_fake.float().mean().item()
-
-        print('Batch Num: [{}/{}], Epoch: [{}/{}]'.format(batch_idx, num_batches, epoch, num_epochs))
-        print('Discriminator Loss: {:.4f}, Generator Loss: {:.4f}'.format(crt_loss, gen_loss))
-        if acc_real and acc_fake:
-            print('D(x): {:.4f}, D(G(z)): {:.4f}'.format(acc_real, acc_fake))
-
     def log(self, crt_loss, gen_loss, acc_real=None, acc_fake=None):
         """
         Logging values
@@ -79,23 +52,20 @@ class MetricLogger:
 
         wandb.log({'d_loss': crt_loss, 'g_loss': gen_loss, 'D(x)': acc_real, 'D(G(z))': acc_fake})
 
-    def log_image(self, images, num_samples, epoch, batch_idx, num_batches, normalize=True):
+    def log_image(self, images, num_samples, epoch, stage, normalize=True):
         """
         Create image grid and save it
         :param images: ``Tor    ch.Tensor(N,C,H,W)``, tensor of images
         :param num_samples: ``int``, number of samples
         :param normalize: if True normalize images
         :param epoch: ``int``, current epoch
-        :param batch_idx: ``int``, current batch
-        :param num_batches: ``int``, numbers bathes
         """
-        images = images[:num_samples, ...]
         nrows = int(np.sqrt(num_samples))
         grid = torchvision.utils.make_grid(images, nrow=nrows, normalize=normalize, scale_each=True)
-        self.save_torch_images(grid, epoch, batch_idx, num_batches)
+        self.save_torch_images(grid, epoch, stage)
         wandb.log({'fixed_noise': [wandb.Image(np.moveaxis(grid.detach().cpu().numpy(), 0, -1))]})
 
-    def save_torch_images(self, grid, epoch, batch_idx, num_batches):
+    def save_torch_images(self, grid, epoch, stage):
         """
         Display and save image grid
         :param grid: ``ndarray``, grid image
@@ -104,14 +74,14 @@ class MetricLogger:
         :param num_batches: ``int``, numbers bathes
         """
         out_dir = self.data_subdir
-        fig = plt.figure(figsize=(16, 16))
+        fig = plt.figure(figsize=(8, 8))
         plt.imshow(np.moveaxis(grid.detach().cpu().numpy(), 0, -1), aspect='auto')
         plt.axis('off')
-        MetricLogger._save_images(fig, out_dir, epoch, batch_idx, num_batches)
+        MetricLogger._save_images(fig, out_dir, epoch, stage)
         plt.close()
 
     @staticmethod
-    def _save_images(fig, out_dir, epoch, batch_idx, num_batches):
+    def _save_images(fig, out_dir, epoch, stage):
         """
         Saves image on drive
         :param fig: pls.figure object
@@ -121,8 +91,7 @@ class MetricLogger:
         :param num_batches: ``int``, numbers bathes
         """
         MetricLogger._make_dir(out_dir)
-        image_name = f"epoch({str(epoch).zfill(len(str(cfg.NUM_EPOCHS)))})-" \
-                     f"batch({str(batch_idx).zfill(len(str(num_batches)))}).jpg"
+        image_name = f"epoch{epoch}_{stage}.jpg"
         fig.savefig('{}/{}'.format(out_dir, image_name))
 
     @staticmethod
