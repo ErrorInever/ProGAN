7415fe45f12f95a014b634df4f0265c2ea77cba7
ErrorInever <errorinever@gmail.com>
2021-05-10 14:13:00 +0000

    add latent space interpolation

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import time
 import torch
 import logging
 import os
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
 from math import log2
 from config import cfg
 from data.dataset import AnimeFacesDataset
@@ -80,6 +83,13 @@ def load_checkpoint(checkpoint_file, model, optimizer, lr):
     logger.info(f"Success loaded model from {checkpoint_file}")
 
 
+def load_gen(gen, filename, device):
+    print("=> Load generator...")
+    cp = torch.load(filename, map_location=device)
+    gen.load_state_dict(cp['state_dict'])
+    print(f"=> Generator model loaded from {filename}")
+
+
 def save_fixed_noise(fixed_noise, filename="fixed_noise.pth.tar"):
     logger.info(f"Saving fixed noise...")
     fixed_noise = {'fixed_noise': fixed_noise}
@@ -92,3 +102,55 @@ def load_fixed_noise(checkpoint_file):
     checkpoint = torch.load(checkpoint_file, map_location="cuda")
     logger.info(f"Success loaded fixed noise from {checkpoint_file}")
     return checkpoint['fixed_noise']
+
+
+def get_random_noise(size, dim, device):
+    """
+     Get random noise from normal distribution
+    :param size: ``int``, number of samples (batch)
+    :param dim: ``int``, dimension
+    :param device: cuda or cpu device
+    :return: Tensor([size, dim, 1, 1])
+    """
+    return torch.randn(size, dim, 1, 1).to(device)
+
+
+def latent_space_interpolation_sequence(latent_seq, step_interpolation=5):
+    """
+    Interpolation between noises
+    :param latent_seq: Tensor([N, z_dim, 1, 1])
+    :param step_interpolation: ``int``: number of steps between each images
+    :return: List([samples, z_dim, 1, 1]
+    """
+    vector = []
+    alpha_values = np.linspace(0, 1, step_interpolation)
+
+    start_idxs = [i for i in range(0, len(latent_seq))]
+    end_idxs = [i for i in range(1, len(latent_seq))]
+
+    for start_idx, end_idx in zip(start_idxs, end_idxs):
+        latent_start = latent_seq[start_idx].unsqueeze(0)
+        latent_end = latent_seq[end_idx].unsqueeze(0)
+        for alpha in alpha_values:
+            vector.append(alpha*latent_end + (1.0 - alpha)*latent_start)
+    return torch.cat(vector, dim=0)
+
+
+def show_batch(batch, save, num_samples=36, figsize=(10, 10), normalize=True):
+    """
+    Show image
+    :param batch: ``Tensor([N, channels, size, size])`` batch
+    :param num_samples: ``int``: number of sumples
+    :param figsize: ``Tuple(n, n)``: size of image
+    :param normalize: if need denormalization
+    """
+    images = batch[:num_samples, ...]
+    nrows = int(np.sqrt(num_samples))
+    grid = torchvision.utils.make_grid(images, nrow=nrows, normalize=normalize, scale_each=True)
+    fig = plt.figure(figsize=figsize)
+    plt.imshow(np.moveaxis(grid.detach().cpu().numpy(), 0, -1))
+    plt.axis('off')
+
+    if save:
+        save_path = os.path.join(save, 'grid_result.png')
+        plt.savefig(save_path)
